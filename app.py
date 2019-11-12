5de69e2e84caa2016c1ebecfb690419e0335d591
pontinlajten <37069466+pontinlajten@users.noreply.github.com>
2019-11-12 21:46:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
-@app.route('/')
+app.config['SECRET_KEY'] = 'bdb878ef8ea259ef877a3686726cf4f9'
 
-def index():
-	return "Hello, world!"
 
-if __name__ == "__main__":
-	app.run(debug=True)
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/products")
+def about():
+    return render_template('products.html')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.email.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
