94b0fcb4222e117c3d6920bd072adb0f2efd20ce
BOBO <chopan-7@student.ltu.se>
2019-11-13 01:10:09 +0100

    aws key added

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
@@ -34,3 +35,34 @@ def login():
 
 if __name__ == '__main__':
     app.run(debug=True)
+=======
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
+app = Flask(__name__)
+
+#MYSQL CONNECTION SETTINGS
+
+# app.config['MYSQL_HOST'] = 'sql7.freemysqlhosting.net'
+# app.config['MYSQL_USER'] = 'sql7311847'
+# app.config['MYSQL_PASSWORD'] = '5h2gDVdPYS'
+# app.config['MYSQL_DB'] = 'sql7311847'
+# app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+app.config['MYSQL_HOST'] = 'gr30db.database.windows.net'
+app.config['MYSQL_PORT'] = 1433
+app.config['MYSQL_USER'] = 'gr30adm'
+app.config['MYSQL_PASSWORD'] = 'Choss!95'
+app.config['MYSQL_DB'] = 'gr30db'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
+
+@app.route('/')
+def index():
+	cur = mysql.connection.cursor()
+	cur.execute('''CREATE TABLE example (id INTEGER, name VARCHAR(20))''')
+	return 'done'
+
+if __name__ == "__main__":
+	app.run(debug = True)
+>>>>>>> aws key added
