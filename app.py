e7b97a1b3807578d4ed03a16e4df796e4e06e2e6
pontinlajten <37069466+pontinlajten@users.noreply.github.com>
2019-11-26 21:12:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         cur = mysql.connection.cursor()
-        cur.execute('SELECT * FROM customers WHERE email = str(form.email.data)')
+        cur.execute('SELECT * FROM customers WHERE str(form.email.data) = email')
         data = cur.fetchall()
 
         if len(data) > 0:
