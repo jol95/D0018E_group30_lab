0575b2bc13515c4e1663f2cab7807f8c66d6b594
pontinlajten <37069466+pontinlajten@users.noreply.github.com>
2019-11-26 12:41:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,13 @@
-from flask import Flask, render_template, url_for, flash, redirect, request
+from flask import *
 from flask_mysqldb import MySQL
 from forms import RegistrationForm, LoginForm
 from werkzeug.security import generate_password_hash, check_password_hash
+from SQLfunctions import *
 
-mysql = MySQL()
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'bdb878ef8ea259ef877a3686726cf4f9'
-
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_PORT'] = 3306
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Choss!95'
-app.config['MYSQL_DB'] = 'webshop'
-#app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-mysql.init_app(app)
-
 
 
 @app.route("/")
@@ -26,24 +18,26 @@ def home():
 
 @app.route("/products")
 def products():
-    return render_template('products.html')
+    args = request.args
+    item = getRow('products', 'prodID=' + args.get("id"))
+    return render_template('products.html', item=item)
 
-
+#flash(f'Account created for {form.email.data}!', 'success')
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    if form.validate_on_submit():
-        conn = mysql.connect()
-        cursor = conn.cursor()
-        hashed_pwd = generate_password_hash(form.password.data)
-        cursor.callproc('sp_createCustomer', (form.first_name.data, form.last_name.data, form.email.data, hashed_pwd, form.home_address.data, form.post_code.data, form.country.data, form.phone_number.data))
-        data = cursor.fetchall()
-        if len(data) is 0:
-            conn.commit()
-            flash(f'Account created for {form.email.data}!', 'success')
-        cursor.close()
-        conn.close()
+    try:
+        if form.validate_on_submit():
+            values = '%s, %s, %s, %s, %s, %s, %s, %s ' % (form.first_name.data, form.last_name.data, form.email.data, form.password.data, form.home_address.data, form.post_code.data, form.country.data, form.phone_number.data)
+            attr = 'firstname, lastname, email, password, address, postcode, country, phoneno'
+            if exist('Customers', 'email='+form.email.data):
+                return json.dumps({'email already exists'})
+            else:
+                insertTo('cart', attr, values)
+                flash(f'Account created for {form.email.data}!', 'success')
         return redirect(url_for('home'))
+    except Exception as e:
+        return json.dumps({'error': str(e)})
     return render_template('register.html', form=form)
 
 
@@ -57,6 +51,41 @@ def login():
         else:
             flash('Invalid Email Or Password', 'danger')
     return render_template('login.html', form=form)
+
+
+@app.route('/addtocart')
+def addtocart():
+    # redirect user to login page if not logged in
+    if 'userid' not in session:
+        flash('Please log in or create an account.')
+        return redirect('/login')
+
+    # attribute data for shoping cart
+    custID = str(session['userid'])  # temporary customer ID (unregistred user)
+    prodID = str(request.args.get('id'))  # productID
+    qty = str(request.args.get('qty'))  # quantity
+    cond = 'custID = ' + custID + ' AND prodID = ' + prodID  # insert/update condition
+
+    # check if the product already exists in users cart
+    if exist('cart', cond):
+        inCart = getRow('cart', cond)
+        # increase the quantity of existing product
+        addqty = str(int(inCart[3]) + int(qty))
+        updateIn('cart', 'qty', addqty, cond)
+    else:
+        # add new product to the cart
+        attr = 'custID, prodID, qty'
+        values = '%s, %s, %s' % (custID, prodID, qty)
+        insertTo('cart', attr, values)
+
+    flash('Product has been added to the shopping cart.')
+    return redirect('/product?id=' + prodID)
+
+@app.route('/startsess')
+def startsess():
+    session['userid'] = 1891
+    return redirect('/')
+
 
 
 if __name__ == '__main__':
