93426fc3574739e6fc472d62746296139aad3eb5
pontinlajten <37069466+pontinlajten@users.noreply.github.com>
2019-12-05 21:05:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -388,19 +388,18 @@ def customerMypage():
                 filename = secure_filename(image.filename)
                 image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
                 profile_pic = str(filename)
-                print "SPARAT"
-                print str(filename)
+
             else:
                 flash('File Extention Is Not Allowed', 'success')
 
 
-        fname = str(form.first_name.data)
-        lname = str(form.last_name.data)
-        email = str(form.email.data)
-        addr = str(form.home_address.data)
-        pcode = str(form.post_code.data)
-        country = str(form.country.data)
-        phone = str(form.phone_number.data)
+        fname = form.first_name.data
+        lname = form.last_name.data
+        email = form.email.data
+        addr = form.home_address.data
+        pcode = form.post_code.data
+        country = form.country.data
+        phone = form.phone_number.data
         update = 'a.firstname="%s", a.lastname="%s", a.email="%s", a.address="%s", a.postcode="%s", \
                    a.country="%s", a.phoneno="%s", a.profilepic="%s"' % (fname, lname, email, addr, pcode, country, phone, profile_pic)
 
