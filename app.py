d9404cd2168570398849a10f3b6aaa32dd4f5701
pontinlajten <37069466+pontinlajten@users.noreply.github.com>
2019-12-13 00:04:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ app.config['ALLOWED_IMAGE_EXTENSIONS'] = ["JPG", "PNG"]
 
 mysql = MySQL(app)
 
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'simonpontinltu@gmail.com'
